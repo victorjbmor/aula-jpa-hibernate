e5594b0f0eb36c2a004b0bfc96be9267c0c268eb
Joao Moraes <victorjbmoraes@gmail.com>
2026-05-18 16:22:28 -0300

    Aula JPA-HIBERNATE-MAVEN

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.Pessoa;
+
+public class Program {
+
+	public static void main(String [] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
+	
+}
